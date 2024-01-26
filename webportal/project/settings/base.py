eab7969f645e32b2eeef2528665d0e114b8a37b1
Ravi <Ravinder@trybz.com.au>
2024-01-26 21:06:51 +1000

    updated base

--- webportal/project/settings/base.py
+++ webportal/project/settings/base.py
@@ -1,3 +1,9 @@
+# webportal/project/settings/base.py
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / "subdir".
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+
 SECRET_KEY = NotImplemented
 DEBUG = False
 
