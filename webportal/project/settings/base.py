7f7fd4448d3284addee2a6e54e9c9b1003f67246
Ravi <Ravinder@trybz.com.au>
2024-01-27 17:04:32 +1000

    base

--- webportal/project/settings/base.py
+++ webportal/project/settings/base.py
@@ -100,7 +100,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'  # Changed from 'static/' to '/static/'
-STATIC_ROOT = '/opt/project/staticfiles'  # type: ignore # noqa: F821
+STATIC_ROOT = '/opt/project/staticfiles/'  # type: ignore # noqa: F821
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
